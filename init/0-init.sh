4613ce3a406171d5e701c22fddce3bc5baa51d9d
teachmyslef <teachmyself@126.com>
2019-09-24 00:50:30 +0800

    Update initilize scripts.

--- init/0-init.sh
+++ init/0-init.sh
@@ -8,7 +8,7 @@ MYSQL_DB_PASS="${MYSQL_DB_PASS:=$MYSQL_ROOT_PASSWORD}"
 MYSQL_CREATE_DBS="${MYSQL_CREATE_DBS//,/  }" # test1,test2,test3
 
 
-[ ${MYSQL_DB_DEBUG} -eq 1 ] && set -xue
+[ ${MYSQL_DB_DEBUG} -eq 1 ] && set -x
 
 
 if [ -z "${MYSQL_DB_HOST}" -o -z "${MYSQL_DB_PASS}" -o -z "MYSQL_CREATE_DBS" ]; then
@@ -21,7 +21,6 @@ else
         for db in ${MYSQL_CREATE_DBS}
         do
             mysql -h ${MYSQL_DB_HOST} -uroot -p${MYSQL_DB_PASS} -e "CREATE DATABASE IF NOT EXISTS ${db} DEFAULT CHARACTER SET utf8;"
-            echo "CREATE DATABASE IF NOT EXISTS ${db} DEFAULT CHARACTER SET utf8;"  >> /docker-entrypoint-initdb.d/9-init.sql;
             if [ $? -ne 0 ]; then
                 echo "[$(date '+%F %T%z')] Create database failed."
             fi
--- init/1-init.sql
+++ init/1-init.sql
@@ -3,7 +3,7 @@
 SELECT CONCAT('0: ', CURRENT_TIMESTAMP()) INTO OUTFILE '/var/lib/mysql-files/0.start.log';
 
 -- -- Create user
-CREATE USER 'mysqld-exporter'@'mysql' IDENTIFIED BY 'mysqld-exporter-password' WITH MAX_USER_CONNECTIONS 3;
+CREATE USER 'mysqld-exporter'@'%' IDENTIFIED BY 'mysqld-exporter-password' WITH MAX_USER_CONNECTIONS 3; FLUSH PRIVILEGES; 
 GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'mysqld-exporter'@'%';
 SELECT CONCAT('1: ', CURRENT_TIMESTAMP()) INTO OUTFILE '/var/lib/mysql-files/1.log';
 
